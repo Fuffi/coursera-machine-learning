09e41f7e30bbb0f7c79233e425c9f190c8058cc5
Federico <ufinii@gmail.com>
2014-03-15 16:14:29 +0000

    gradient descent for multiple vars

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -9,28 +9,32 @@ J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCostMulti) and gradient here.
-    %
+  % ====================== YOUR CODE HERE ======================
+  % Instructions: Perform a single gradient step on the parameter vector
+  %               theta. 
+  %
+  % Hint: While debugging, it can be useful to print out the values
+  %       of the cost function (computeCostMulti) and gradient here.
+  %
 
+  new_theta = theta;
+  theta_length = length(theta);
+  for j = 1:theta_length
+    theta_step = 0;
+    for i = 1:m
+      xi = X(i, :);
+      hypothesis = xi * theta;
+      theta_step += (hypothesis - y(i)) * xi(j);
+    end
+    new_theta(j) = theta(j) - (alpha * theta_step / m);
+  end
 
+  theta = new_theta;
 
+  % ============================================================
 
-
-
-
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
-    J_history(iter) = computeCostMulti(X, y, theta);
+  % Save the cost J in every iteration    
+  J_history(iter) = computeCostMulti(X, y, theta);
 
 end
 
