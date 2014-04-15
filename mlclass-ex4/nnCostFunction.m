3c1ad55e50309135724e1e6837e5a950394a6e8c
Federico <ufinii@gmail.com>
2014-04-15 22:47:13 +0100

    backpropagation working

--- mlclass-ex4/nnCostFunction.m
+++ mlclass-ex4/nnCostFunction.m
@@ -62,24 +62,38 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
-A1 = [ones(m, 1) X];
-
-Z2 = A1 * Theta1';
-A2 = [ones(m, 1) sigmoid(Z2)];
-
-Z3 = A2 * Theta2';
-A3 = sigmoid(Z3);
-
 training_set_y = [];
 for n=1:num_labels
   training_set_y = [training_set_y n==y];
 end
 
+Delta_1 = 0;
+Delta_2 = 0;
+
 for i=1:m
+  xi = X(i, :);
   yi = training_set_y(i, :);
-  hypothesis = A3(i, :);
+  a1 = [1 xi];
+  z2 = a1 * Theta1';
+  a2 = [1 sigmoid(z2)];
+  z3 = a2 * Theta2';
+  a3 = sigmoid(z3);
+
+  hypothesis = a3;
+
   J += 1/m * sum(-yi .* log(hypothesis) - (1 - yi) .* log(1 - hypothesis));
+
+  delta_3 = a3 - yi;
+  delta_2 = delta_3 * Theta2;
+  delta_2 = delta_2(2:end) .* sigmoidGradient(z2);
+
+  Delta_1 = Delta_1 + delta_2' * a1;
+  Delta_2 = Delta_2 + delta_3' * a2;
+
 end
+
+Theta1_grad = Delta_1/m;
+Theta2_grad = Delta_2/m;
 
 reg = (lambda / (2 * m)) * (sumsq(Theta1(:, 2:end)(:)) + sumsq(Theta2(:, 2:end)(:)));
 
